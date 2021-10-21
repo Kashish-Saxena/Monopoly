5f46aeee90fdb7543a7d52c6c61975ddb486bd1e
Nicholas Thibault <nicholas_thibault@hotmail.com>
2021-10-21 18:37:53 -0400

    Adding Command, CommandWord and CommandWords classes.

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -1,2 +1,36 @@
-public enum CommandWord {
+/**
+ * This class is taken from the Zuul with enumerations v2 project from the lectures
+ * with some modifications to work with the monopoly project.
+ *
+ * Representations for all the valid command words for the game
+ * along with a string in a particular language.
+ *
+ * @author  Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+public enum CommandWord
+{
+    // A value for each command word along with its
+    // corresponding user interface string.
+    PLAYER_STATE("state"), BUY_PROPERTY("buy"), PASS_TURN("pass"), UNKNOWN("?");;
+
+    // The command string.
+    private String commandString;
+
+    /**
+     * Initialise with the corresponding command string.
+     * @param commandString The command string.
+     */
+    CommandWord(String commandString)
+    {
+        this.commandString = commandString;
+    }
+
+    /**
+     * @return The command word as a string.
+     */
+    public String toString()
+    {
+        return commandString;
+    }
 }
