9fe9bbdbe574dc2cd6811226b4659a203b45eb09
Sahil <sahilagrawal@cmail.carleton.ca>
2021-10-21 16:43:10 -0400

    Added comments and documentation

--- src/Monopoly/Player.java
+++ src/Monopoly/Player.java
@@ -1,3 +1,8 @@
+/**
+ * Monopoly.Player class creates a player for the game
+ * @author: Sahil Agrawal
+ * @version: October 21 2020
+ */
 package Monopoly;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +14,13 @@ public class Player {
     public int money;
     public int currentPosition;
 
+    /**
+     * Constructor for the player
+     *
+     * @param playerName PLayer name
+     * @param money inital money that the player starts with
+     * @param currentPosition the position of the player on the board
+     */
     public Player(String playerName, int money, int currentPosition){
         this.playerName = playerName;
         this.money = money;
@@ -16,19 +28,36 @@ public class Player {
         ownedProperties = new HashMap<>();
     }
 
+    /**
+     * Returns the name of the player
+     * @return Syring of the player name
+     */
     public String getPlayerName() {
         return playerName;
     }
 
+    /**
+     * Returns the properties owned by a player
+     * @return properties owned by a player
+     */
     public HashMap<Player, ArrayList<Property>> getOwnedProperties() {
         return this.ownedProperties;
 
     }
 
+    /**
+     * Returns how much money a player has
+     * @return int of players money
+     */
     public int getMoney() {
         return money;
     }
 
+    /**
+     * Adds a property to a player if they decide to buy it
+     * @param player the player that wants to buy
+     * @param property the property they want to buy
+     */
     private void buyProperty(Player player,Property property){
         ArrayList<Property> properties = ownedProperties.get(player);
         if(properties == null){
@@ -39,16 +68,29 @@ public class Player {
         }
 
     }
+
+    /**
+     * Updates the players position
+     * @param amount how many spaces the player moves
+     */
     private void updateCurrentPosition(int amount){
         currentPosition += amount;
     }
 
-
+    /**
+     * The String representation of Monopoly.Player
+     * @return String representation of Monopoly.Player
+     */
     @Override
     public String toString(){
         return "Player name: " + playerName + "\nOwned Properties: " + ownedProperties + "\nMoney in the bank: " + money + "\nCurrent Position: " + currentPosition;
     }
 
+    /**
+     * Compares all the values of the players
+     * @param o the object that is being compared with
+     * @return boolean that will return the result of the comparison
+     */
     @Override
     public boolean equals(Object o){
         if(this == o)return true;
