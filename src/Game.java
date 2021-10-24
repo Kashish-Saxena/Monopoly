26816fdb39f2570bae50cc076d15e593fa7210fe
Kashish708 <s.kashish0123@gmail.com>
2021-10-24 12:29:56 -0400

    Started Game Class

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,17 @@
+import java.util.*;
+
 public class Game {
+
+    private List<Player> players;
+    private int currentPlayerIndex;
+    private CommandWord commandWord;
+    private int numPlayers;
+    private ArrayList availableProperties;
+    private Player currentPlayer;
+    private InputParser parser;
+    private List<Integer> dice;
+
+    public Game(){
+
+    }
 }
